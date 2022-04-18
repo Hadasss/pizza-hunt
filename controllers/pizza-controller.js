e5298c4a3d51258a90ddf242ac33352e23894f87
Hadasss <hadas.gadish@gmail.com>
2022-04-18 14:57:58 -0700

    added api pizza routes + fetch post pizza in add-pizza.js

---
+++ controllers/pizza-controller.js
@@ -0,0 +1,69 @@
+const { Pizza } = require("../models");
+
+const pizzaController = {
+  // the functions will go here as methods
+
+  // get all pizzas
+  getAllPizza(req, res) {
+    Pizza.find({})
+      .then((dbPizzaData) => res.json(dbPizzaData))
+      .catch((err) => {
+        console.log(err);
+        res.status(400).json(err);
+      });
+  },
+
+  // get one pizza by id
+  // Instead of accessing the entire req, we've destructured params out of it
+  getPizzaById({ params }, res) {
+    Pizza.findOne({ _id: params.id })
+      .then((dbPizzaData) => {
+        if (!dbPizzaData) {
+          res.status(404).json({ message: "No pizza was found with this id" });
+          return;
+        }
+        res.json(dbPizzaData);
+      })
+      .catch((err) => {
+        console.log(err);
+        res.status(400).json(err);
+      });
+  },
+
+  // create pizza
+  // we destructure the body out of the Express.js req object because we don't need to interface with any of the other data it provides.
+  createPizza({ body }, res) {
+    Pizza.create(body)
+      .then((dbPizzaData) => res.json(dbPizzaData))
+      .catch((err) => res.status(400).json(err));
+  },
+
+  // update pizza
+  updatePizza({ params, body }, res) {
+    // {new: true} will return the updated document. without it mongoose will return the original document.
+    Pizza.findByIdAndUpdate({ _id: params.id }, body, { new: true })
+      .then((dbPizzaData) => {
+        if (!dbPizzaData) {
+          res.status(404).json({ message: "No pizza was found with this id" });
+          return;
+        }
+        res.json(dbPizzaData);
+      })
+      .catch((err) => res.status(400).json(err));
+  },
+
+  // delete pizza
+  deletePizza({ params }, res) {
+    Pizza.findOneAndDelete({ _id: params.id })
+      .then((dbPizzaData) => {
+        if (!dbPizzaData) {
+          res.status(404).json({ message: "No pizza was found with this id" });
+          return;
+        }
+        res.json(dbPizzaData);
+      })
+      .catch((err) => res.status(400).json(err));
+  },
+};
+
+module.exports = pizzaController;
